b7c78ac9aca0f0bf26bd5c43b451c79195490d86
ZussiMane <igoryunja@gmail.com>
2024-03-13 14:35:04 +0300

    Обновил файлы после написание решения задачи

--- obj/Debug/net8.0/Sem2_Task1.AssemblyInfo.cs
+++ obj/Debug/net8.0/Sem2_Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Sem2_Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8db7e56f938844a1bf7bb763295f4cdb67113cf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5067aa5bc20e39842525dad7b884793471cb3e97")]
 [assembly: System.Reflection.AssemblyProductAttribute("Sem2_Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Sem2_Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
